f85c029d605c2d87d39e18c7e042c71c37753797
meraj2k <meraj2k22@gmail.com>
2024-09-01 00:59:26 +0530

    deployment-setup

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const eventRoutes = require('./routes/eventRoutes');
+const { successResponse } = require('./utils/responseHandler');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/", (req, res) => {
+    successResponse(res, { message: "Hellow World!" })
+})
 
 app.use('/api/events', eventRoutes);
 
